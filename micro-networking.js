da220bbdca498aa241d114f1b4dbcc721d0597d8
Artucuno <28388670+Artucuno@users.noreply.github.com>
2022-05-24 11:36:25 +1000

    v2 - Updated js

--- micro-networking.js
+++ micro-networking.js
@@ -15,24 +15,45 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
-// Selects a random number and shows the connection code
-function createServer () {
-    connectionNum = randint(0, 10)
+// createServer Function
+// LED (True/False) - Enable LED light once connected
+// group (int) - Set radio group
+// min (int) - Set minimum random number
+// max (int) - Set maximum random number
+// 
+function createServer (LED: boolean, group: number, min: number, max: number) {
+    radio.setGroup(group)
+    radio.setTransmitPower(7)
+    connectionNum = randint(min, max)
     basic.showNumber(connectionNum)
-    basic.pause(500)
+    basic.pause(200)
     // Guesses the other microbit connectionNum
     while (!(isConnected)) {
-        radio.sendValue("connectionCode", randint(0, 10))
-        basic.pause(randint(400, 500))
+        radio.sendValue("connectionCode", randint(min, max))
+        basic.pause(randint(50, 100))
     }
-    basic.showString("Connected!")
+    // Turns on an LED light once connected. Can be used for anything.
+    // (Set the write pin)
+    if (LED) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+    basic.showIcon(IconNames.Yes)
 }
+// Created by Artucuno#1898
+// 
+// https://github.com/Artucuno
 let isServer = 0
 let connectionNum = 0
 let isConnected = 0
 radio.sendString("")
 basic.showNumber(0)
-createServer()
+// createServer Function
+// LED (True/False) - Enable LED light once connected
+// group (int) - Set radio group
+// min (int) - Set minimum random number
+// max (int) - Set maximum random number
+// 
+createServer(false, 87, 0, 10)
 // Displays which microbit is the host.
 // 0 = Client
 // 1 = Host
